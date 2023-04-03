ef3e00d53a9fa6b3e323626e29ced195dc0c7df0
Romenick <romenick.tester@gmail.com>
2023-04-04 00:14:20 +0100

    SERVER: create error handlers + install express async handler

--- products/server.js
+++ products/server.js
@@ -1,34 +1,35 @@
 require("dotenv").config();
 const express = require("express");
-const server = express();
+const asyncHandler = require("express-async-handler");
 const Product = require("./assets/models/Product");
 const connectDB = require("./assets/connectDB");
+const { notFound, errorCatcher } = require("./assets/middlewares/errorHandlers");
 const port = process.env.PRODUCT_PORT || 4001;
 
+const server = express();
 connectDB("Product");
 
 server.use(express.json());
 
-server.get("/all", async (req, res) => {
-    try {
-        const products = await Product.find();
+server.get("/all", asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+}));
 
-        res.json(products);
-    } catch (err) {
-        console.error(`Error: ${err.message}`);
+server.get("/single/:id", asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        throw new Error("Product not found!");
     }
-});
 
-server.get("/single/:id", async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
+    res.json(product);
+}));
 
-        res.json(product);
-    } catch (err) {
-        console.error(`Error: ${err.message}`);
-    }
-});
+server.use(notFound);
+
+server.use(errorCatcher);
 
 server.listen(port, () => {
-    console.log(`Product server started running on port: ${port}`);
+    console.log(`Product server running in ${process.env.NODE_ENV} mode on port: ${port}`);
 });
